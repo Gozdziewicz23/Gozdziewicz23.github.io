49b0050dde15178199e07180b6ef82aef76fd728
Gozdziewicz23 <uf20074@bristol.ac.uk>
2023-01-06 13:09:20 +0100

    fertility rate in Poland

--- project.js
+++ project.js
@@ -1,7 +1,9 @@
 //Define charts:
 var fertilityCountriesRates = "charts/fertilityCountriesRates.json";
 var fertilityWorldRates = "charts/fertilityWorldRates.json";
+var fertilityRatePoland = "charts/fertilityRatePoland.json";
 
 // Embed charts:
 vegaEmbed('#fertilityCountriesRates', fertilityCountriesRates);
 vegaEmbed('#fertilityWorldRates', fertilityWorldRates);
+vegaEmbed('#fertilityRatePoland', fertilityRatePoland);
