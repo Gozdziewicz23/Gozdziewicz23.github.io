261d676562ffcf41a08c637709f2daf71e1f6447
Gozdziewicz23 <uf20074@bristol.ac.uk>
2023-01-06 12:24:22 +0100

    first completed - year selector is not working

---
+++ project.js
@@ -0,0 +1,7 @@
+//Define charts:
+var fertilityCountriesRates = "charts/fertilityCountriesRates.json";
+var fertilityWorldRates = "charts/fertilityWorldRates.json";
+
+// Embed charts:
+vegaEmbed('#fertilityCountriesRates', fertilityCountriesRates);
+vegaEmbed('#fertilityWorldRates', fertilityWorldRates);
