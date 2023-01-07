6b9fc3193010ea5ba8cf340ac6f91d538b6ad4ad
Gozdziewicz23 <uf20074@bristol.ac.uk>
2023-01-07 16:16:54 +0100

    realProblem chart tryings

--- project.js
+++ project.js
@@ -3,9 +3,11 @@ var fertilityCountriesRates = "charts/fertilityCountriesRates.json";
 var fertilityWorldRates = "charts/fertilityWorldRates.json";
 var fertilityRatePoland = "charts/fertilityRatePoland.json";
 var fertilityCompared = "charts/fertilityCompared.json";
+var realProblem = "charts/realProblem.json";
 
 // Embed charts:
 vegaEmbed('#fertilityCountriesRates', fertilityCountriesRates);
 vegaEmbed('#fertilityWorldRates', fertilityWorldRates);
 vegaEmbed('#fertilityRatePoland', fertilityRatePoland);
 vegaEmbed('#fertilityCompared', fertilityCompared);
+vegaEmbed('#realProblem', realProblem);
