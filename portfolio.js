35767acdccd98a1ce039f0d192047b51030a5785
Gozdziewicz23 <119769354+Gozdziewicz23@users.noreply.github.com>
2023-01-08 20:54:53 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -3,9 +3,17 @@ var myChart1 = "chart1_ENV.json";
 var myChart2 = "chart2_Species.json";
 var myChart3 = "Chart3_IMF.json";
 var myChart4 = "Chart4_IMF%.json";
+var myChart5 = "Chart5_.json";
+var myChart6 = "Chart6_.json";
+var myChart7 = "Chart7.json";
+var myChart8 = "Chart8.json";
 
 // Embed charts:
 vegaEmbed('#Chart1', myChart1);
 vegaEmbed('#Chart2', myChart2);
 vegaEmbed('#Chart3', myChart3);
 vegaEmbed('#Chart4', myChart4);
+vegaEmbed('#Chart5', myChart5);
+vegaEmbed('#Chart6', myChart6);
+vegaEmbed('#Chart7', myChart7);
+vegaEmbed('#Chart8', myChart8);
